ade069297524100525e4ed05174292855d3e7632
Jonatasdss <54269600+Jonatasdss@users.noreply.github.com>
2021-08-12 20:30:06 -0300

    Add files via upload
    Primeiro dia de aula

---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+/* 
+ nome: Jonatas dos Santos de Sales
+ matricula: 0050015649 
+*/ 
+public class Tabuada {
+    public static void main(String[] args) {
+        int x = 10;
+        for (int i=1; i<=x; i++)
+        {
+          int resul;
+          resul = x * i;
+          System.out.println(resul);
+        }
+    }
+}
